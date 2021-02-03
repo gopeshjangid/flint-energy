6cf934068e15e6e8a572b91b5d026aac1c65109d
Mohit Gupta <mohit.gupta30000@gmail.com>
2021-02-03 10:55:35 +0530

    fixing console errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,26 @@
-import React ,{useState} from 'react';
-import { ThemeProvider } from 'theme-ui';
-import theme from 'theme';
-import SEO from 'components/seo';
-import Layout from 'components/layout';
-import Banner from 'sections/banner';
-import FeaturedSpace from 'sections/featured-space';
-import OurCustomer from 'sections/our-customer';
-import Gallery from 'sections/gallery';
-import ContactForm from 'sections/ContactForm';
-import Blog from 'sections/blog';
-import Subscription from 'sections/subscription';
+import React, { useState } from "react";
+import { ThemeProvider } from "theme-ui";
+import theme from "theme";
+import SEO from "components/seo";
+import Layout from "components/layout";
+import Banner from "sections/banner";
+import FeaturedSpace from "sections/featured-space";
+import OurCustomer from "sections/our-customer";
+import Gallery from "sections/gallery";
+import ContactForm from "sections/ContactForm";
+import Blog from "sections/blog";
+import Subscription from "sections/subscription";
 
 export default function IndexPage() {
-
-  const [bill , setBill] = useState(3500);
+  const [bill, setBill] = useState(3500);
 
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO title="FlinTech" />
-        <Banner setBillAmount ={setBill} />
+        <Banner setBillAmount={setBill} />
         <ContactForm bill={bill} />
         <Blog />
-      
       </Layout>
     </ThemeProvider>
   );
