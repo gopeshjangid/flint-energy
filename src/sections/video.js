5e25b784f6fab1ad4786f78a38cccd9fa38c3844
Mohit Gupta <mohit.gupta30000@gmail.com>
2021-02-03 10:09:47 +0530

    fix video autoplaying

--- src/sections/video.js
+++ src/sections/video.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
+import { Waypoint } from "react-waypoint";
 import { Box } from "theme-ui";
+
 const AutomaticPlayer = function (props) {
   let [shouldPlay, updatePlayState] = useState(false);
 
@@ -12,17 +14,24 @@ const AutomaticPlayer = function (props) {
   };
 
   return (
-      <Box sx={style.wrapper} >
-        <ReactPlayer width="100%"
-      height="100%" loop={true}   playing={true} url="homepagevideo.mp4" />
+    <Waypoint onEnter={handleEnterViewport} onLeave={handleExitViewport}>
+      <Box sx={style.wrapper}>
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          loop={true}
+          playing={shouldPlay}
+          url="homepagevideo.mp4"
+        />
       </Box>
+    </Waypoint>
   );
 };
 export default AutomaticPlayer;
 
-const style={
-  wrapper  :{
-    margin : '20px',
-    maxWidth : '465px'
-  }  
-}
+const style = {
+  wrapper: {
+    margin: "20px",
+    maxWidth: "465px",
+  },
+};
